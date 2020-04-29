5043072c8eb91bf397c89eb8c85a51134df89b58
Jasper <jiunway@gmail.com>
2020-04-29 17:18:38 +0800

    test

--- express/server.js
+++ express/server.js
@@ -8,6 +8,11 @@ const bodyParser = require('body-parser');
 const line = require('@line/bot-sdk');
 const lineNotify = require('express-line-notify');
 
+var config_line_notify_netlify = {
+  clientId: "ftjvDnrjcE7uhDvT4qJcWS",
+  clientSecret: "CRvbN41rDh7hUUB3r6qp3SOsXJVTzUwldOafUHq32rd"
+}
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, {
@@ -25,6 +30,13 @@ router.get('/getip', (req, res) => {
 
   return res.send("show_ip('" + ip + "');");
 });
+
+app.use('/line_notify_netlify',
+  lineNotify(config_line_notify_netlify),
+  function(req, res) {
+    const token = req['line-notify-access-token'];
+    console.log("token:" + token);
+  });
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
