fb7480dfb2a4d4ba3fb2d613502495468600168c
Jasper <jiunway@gmail.com>
2020-04-29 17:02:47 +0800

    test

--- express/server.js
+++ express/server.js
@@ -5,12 +5,15 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const line = require('@line/bot-sdk');
+const lineNotify = require('express-line-notify');
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  res.write('<h1>Hello from Express.js Jasper!</h1>');
+  res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
 
@@ -23,16 +26,8 @@ router.get('/getip', (req, res) => {
   return res.send("show_ip('" + ip + "');");
 });
 
-// router.get('/another', (req, res) => res.json({
-//   route: req.originalUrl
-// }));
-// router.post('/', (req, res) => res.json({
-//   postBody: req.body
-// }));
-
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
-//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
